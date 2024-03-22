d65258ed125531e018afb7e474abaf6d6be1d9ca
siddh34 <ssiddharthsutar@gmail.com>
2024-03-23 00:37:25 +0530

    added button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,11 +1,25 @@
-function button(props: any) {
-  // extract props
-  
+interface ButtonProps {
+  color: string;
+  isDisabled: boolean;
+  textInside: string;
+}
+
+function button(props: ButtonProps) {
+  const { color, isDisabled, textInside } = props;
+
+  if (isDisabled === true) {
+    return (
+        <button className={`flex items-center bg-${color} opacity-50`} disabled>
+            <span>{textInside}</span>
+        </button>
+    );
+  }
+
   return (
-    <button className="">
-      
+    <button className={`flex items-center bg-${color}`}>
+      <span>{textInside}</span>
     </button>
-  )
+  );
 }
 
 export default button
