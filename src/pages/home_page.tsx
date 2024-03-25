33cd2389208281fdf8ae712b4d02d16cd7358b85
siddh34 <ssiddharthsutar@gmail.com>
2024-03-25 21:14:43 +0530

    done with buttons

--- src/pages/home_page.tsx
+++ src/pages/home_page.tsx
@@ -1,6 +1,5 @@
 import AppButton from "../components/app_button";
 
-
 function home_page() {
     return (
         <>
@@ -14,10 +13,31 @@ function home_page() {
                         Graph Section
                     </p>
                 </div>
-                <div className="flex">
-                    <AppButton color={"pink"} isDisabled={false} textInside={""}/>
-                    <AppButton color={"pink"} isDisabled={false} textInside={""}/>
-                    <AppButton color={"pink"} isDisabled={false} textInside={""}/>
+                <div className="flex flex-col items-center justify-center m-10 bg-red-900 h-80">
+                    <div className="flex">
+                        <button
+                            type="button"
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        >
+                            Start
+                        </button>
+                    </div>
+                    <div className="flex">
+                        <button
+                            type="button"
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        >
+                            Stop
+                        </button>
+                    </div>
+                    <div className="flex">
+                        <button
+                            type="button"
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        >
+                            Break
+                        </button>
+                    </div>
                 </div>
             </div>
         </>
