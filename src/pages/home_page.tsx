24e6bb35fb833a5ae5bdf39aefdd86e86e478298
VidhanshuJ21 <jadhavvidhanshu@gmail.com>
2024-04-18 17:11:03 +0530

    modified:   src/pages/home_page.tsx

--- src/pages/home_page.tsx
+++ src/pages/home_page.tsx
@@ -141,8 +141,16 @@ function home_page() {
                             <TimerToggler time={20} setTimer={setRemainingTimer} />
                             <TimerToggler time={40} setTimer={setRemainingTimer} />
                             <TimerToggler time={60} setTimer={setRemainingTimer} />
+                            <div className="mb-6">
+      <label htmlFor="default-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Default input</label>
+      <input 
+        type="text" 
+        id="default-input" 
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      />
+    </div>  
                         </div>
-
+                        
 
                     </div>
                     <div className="flex">
