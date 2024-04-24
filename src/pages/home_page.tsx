ae04636bd544cc97ab553fa01d7d99c234d75a1d
siddh34 <ssiddharthsutar@gmail.com>
2024-04-25 02:18:41 +0530

    added suggestion button

--- src/pages/home_page.tsx
+++ src/pages/home_page.tsx
@@ -232,6 +232,16 @@ function home_page() {
                                 History
                             </button>
                         </div>
+                        <div className="flex">
+                            <button
+                                onClick={() => {
+
+                                }}
+                                className="text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-teal-600 dark:hover:bg-teal-700 focus:outline-none dark:focus:ring-teal-800"
+                            >
+                                Suggestions?
+                            </button>
+                        </div>
                         <div className="flex flex-col"></div>
                     </div>
                 </div>
