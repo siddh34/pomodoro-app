9fc3ed3d99c4e7685daf847b700d67f3b00baea6
siddh34 <ssiddharthsutar@gmail.com>
2024-05-01 16:31:21 +0530

    added color

--- src/pages/about_us.tsx
+++ src/pages/about_us.tsx
@@ -1,35 +1,57 @@
-import React from 'react';
+import React from "react";
 
 const AboutUsPage: React.FC = () => {
-  return (
-    <div>
-      <h1>About Us</h1>
-      <p> <div className="App">
-      <h1 style={{ fontFamily: 'Roboto Slab, sans-serif' }}>!! WELCOME TO POMODORO !!</h1>
-    </div></p>
-      <p>
-      <div className="App">
-      <h1 style={{ fontFamily: 'Roboto Slab, sans-serif' }}>The Pomodoro Technique is a time management strategy that divides work into manageable chunks called "Pomodoros," which are designed to help people become more productive and focused.  During this time the person can concentrate solely and without interruption on one subject. Following the completion of a Pomodoro, one takes a quick 5-minute break to recuperate.</h1>
-    </div>
-      </p>
-      <p>
-      <div className="App">
-      <h1 style={{ fontFamily: 'Roboto Slab, sans-serif' }}>The Pomodoro Technique is a time management strategy that divides work into manageable chunks called "Pomodoros," which are designed to help people become more productive and focused.  During this time the person can concentrate solely and without interruption on one subject. Following the completion of a Pomodoro, one takes a quick 5-minute break to recuperate.</h1>
-    </div>
-      </p>
-      <p>
-      <div className="App">
-      <h1 style={{ fontFamily: 'Roboto Slab, sans-serif' }}> This application uses Rust for Backend and React JS for frontend </h1>
-    </div>
-      </p>
-      <p>
-      <div className="App">
-      <h1 style={{ fontFamily: 'Roboto Slab, sans-serif' }}> by - 
-      Siddharth. S. Sutar and Vidhanshu. S. Jadhav </h1>
-    </div>
-      </p>
-    </div>
-  );
+    return (
+      <div className="flex flex-col min-h-screen bg-slate-300">
+        
+        <div>
+            <h1>About Us</h1>
+            <p>
+                {" "}
+                <div className="App">
+                    <h1 style={{ fontFamily: "Roboto Slab, sans-serif" }}>
+                        !! WELCOME TO POMODORO !!
+                    </h1>
+                </div>
+            </p>
+            <p>
+                <div className="App">
+                    <h1 style={{ fontFamily: "Roboto Slab, sans-serif" }}>
+                        The Pomodoro Technique is a time management strategy
+                        that divides work into manageable chunks called
+                        "Pomodoros," which are designed to help people become
+                        more productive and focused.  During this time the
+                        person can concentrate solely and without interruption
+                        on one subject. Following the completion of a Pomodoro,
+                        one takes a quick 5-minute break to recuperate.
+                    </h1>
+                </div>
+            </p>
+            <p>
+                <div className="App">
+                    <h1 style={{ fontFamily: "Roboto Slab, sans-serif" }}>
+                        The Pomodoro Technique is a time management strategy
+                        that divides work into manageable chunks called
+                        "Pomodoros," which are designed to help people become
+                        more productive and focused. During this time the person
+                        can concentrate solely and without interruption on one
+                        subject. Following the completion of a Pomodoro, one
+                        takes a quick 5-minute break to recuperate.
+                    </h1>
+                </div>
+            </p>
+            <p>
+                <div className="App">
+                    <h1 style={{ fontFamily: "Roboto Slab, sans-serif" }}>
+                        {" "}
+                        This application uses Rust for Backend and React JS for
+                        frontend{" "}
+                    </h1>
+                </div>
+            </p>
+        </div>
+      </div>
+    );
 };
 
 export default AboutUsPage;
